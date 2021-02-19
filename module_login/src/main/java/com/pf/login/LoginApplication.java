be4cf6160f8d049723c0c8d118a0539266edeb34
bingopf <qq123456>
2021-02-19 09:48:30 +0800

    update temp test

--- module_login/src/main/java/com/pf/login/LoginApplication.java
+++ module_login/src/main/java/com/pf/login/LoginApplication.java
@@ -3,4 +3,10 @@ package com.pf.login;
 import android.app.Application;
 
 public class LoginApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //初始化存储方案
+//        MMKV.initialize(this);
+    }
 }
---
+++ module_login/src/main/java/com/pf/login/LoginTest.java
@@ -0,0 +1,27 @@
+package com.pf.login;
+
+import android.util.Log;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.pf.login.export.bean.LoginInfo;
+import com.pf.login.export.router.LoginRouter;
+import com.pf.login.export.service.ICartService;
+
+/**
+ * 购物车组件服务工具类
+ * 其他组件直接使用此类即可：页面跳转、获取服务。
+ *
+ * @author hufeiyang
+ */
+public class LoginTest {
+
+    /**
+     * 跳转到 xx页面
+     *
+     */
+    public static void temp() {
+        Log.i("login","module_login");
+    }
+
+
+}
